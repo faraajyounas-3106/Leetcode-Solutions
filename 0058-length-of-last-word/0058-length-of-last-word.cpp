e843e73e60d3c4690f6f0f9d62a5587a70e0f919
Faraaj Younas <faraajyounas@gmail.com>
2026-07-03 13:04:57 +0500

    Time: 0 ms (100%), Space: 8.9 MB (34.31%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        bool flag = false;
+        for(int i = s.length() - 1 ; i >= 0 ; i--){
+                if(s[i] != ' '){
+                    len++;
+                    flag = true;
+                }
+                else if(s[i] == ' ' && flag == true){
+                break;
+                }
+        }
+        return len;
+    }
+};
